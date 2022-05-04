a22207603157812db01d6ffde9fec55b61668cd6
Lillie <91358136+LillieH001@users.noreply.github.com>
2022-05-03 20:56:18 -0400

    Refactor

--- src/main/java/com/lizardmanshamanminionspawnsound/LizardmanShamanMinionSpawnSoundConfig.java
+++ src/main/java/com/lizardmanshamanminionspawnsound/LizardmanShamanMinionSpawnSoundConfig.java
@@ -1,11 +1,11 @@
-package com.example;
+package com.lizardmanshamanminionspawnsound;
 
 import net.runelite.client.config.Config;
 import net.runelite.client.config.ConfigGroup;
 import net.runelite.client.config.ConfigItem;
 
 @ConfigGroup("example")
-public interface ExampleConfig extends Config
+public interface LizardmanShamanMinionSpawnSoundConfig extends Config
 {
 	@ConfigItem(
 		keyName = "greeting",
